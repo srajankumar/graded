38fc752130ec3dca1fdd7c5f3de16c81aa9bf3aa
srajan <kumarsrajan02@gmail.com>
2024-06-25 20:58:56 +0530

    add: sign-up

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="flex min-h-[100dvh] justify-center items-center">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
